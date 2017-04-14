30a409008c173c61e4b78ae3621063eb5ac5e786
Sandro Rybarik <sandro.rybarik@gmail.com>
2017-04-14 18:09:02 +0200

    Imported body-parser to application

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express')
 var app = express()
 
+var bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
